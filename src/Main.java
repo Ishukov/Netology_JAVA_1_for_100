6b80e5fd62ca6b68455be18ad6e3865de5751246
s_ishukov <s_ishukov@bk.ru>
2023-01-26 14:24:12 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Объявляете переменные для входных данных и
+           параметров программы: начального счёта,
+           суммы пополнения и тп */
+
+        int balance = 500;
+        int refill = 101;
+        int bonus = (refill > 100) ? refill / 100 : 0;
+        int total_balance = balance + refill + bonus;
+        System.out.println("Сумма на счету: " + balance + " руб.");
+        System.out.println("Сумма пополнения: " + refill + " руб.");
+        System.out.println("Начисленно бонусов: " + bonus);
+        System.out.println("Итоговая сумма на счету: " + total_balance + " руб.");
+
+        /* Условным оператором проверяете, превысила ли
+           сумма пополнения порог, и для этих двух разных
+           сценариев рассчитываете сумму бонуса и выводите
+           на экран. */
+    }
+}
