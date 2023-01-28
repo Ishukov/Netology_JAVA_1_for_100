cd87e38669294fcc358404d252bd1f9b6fda7b5d
s_ishukov <s_ishukov@bk.ru>
2023-01-28 23:37:01 +0500

    condition changed

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
            суммы пополнения и тп */
 
         int balance = 500;
-        int refill = 100;
-        int bonus = (refill >= 100) ? refill / 100 : 0;
+        int refill = 1001;
+        int bonus = (refill >= 1000) ? refill / 100 : 0;
         int totalBalance = balance + refill + bonus;
         System.out.println("Сумма на счету: " + balance + " руб.");
         System.out.println("Сумма пополнения: " + refill + " руб.");
